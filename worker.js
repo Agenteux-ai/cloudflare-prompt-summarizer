9875186d07271de6efaa5f8de99b3fac8d6a541c
Todd Fredrich <toddfredrich@Todds-MacBook-Air.local>
2025-10-21 10:20:42 -0700

    Added worker.js

---
+++ worker.js
@@ -0,0 +1,77 @@
+export default {
+  async fetch(request, env) {
+    // Keep these secret... Include as Authorization header for API Key usage.
+    var apiKeys = [
+    ];
+
+    if (!request.headers.has('Authorization')) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    if (!apiKeys.includes(request.headers.get('Authorization'))) {
+      return new Response("Invalid token", { status: 401 });
+    }
+
+    if (request.method === 'POST') return handlePost(request, env);
+
+    let response = Response.json({ error: "Only POST is supported" });
+    return new Response(response.body, {
+      status: 405
+    });
+  }
+};
+
+async function handlePost(request, env) {
+  let response = {};
+  if (request.headers.has('Content-Length') && parseInt(request.headers.get('Content-Length')) > 0) {
+    const prompt = await request.text();
+
+    // messages - chat style input
+    let system = `You are a prompt summarizing expert. Summarize the user's prompt to derive a \`name\` and \`description\`, returned in JSON format conforming to the following JSON Schema:
+    '''
+    {
+      "type": "object",
+      "properties": {
+        "name": {
+          "type": "string",
+          "maxLength": 128,
+          "description": "A descriptive, human-readable name for the prompt."
+        },
+        "description": {
+          "type": "string",
+          "maxLength": 1024,
+          "description": "A detailed description of what the prompt does."
+        }
+      },
+      "required": [
+        "name",
+        "description"
+      ],
+      "additionalProperties": false
+    }
+    '''
+    Provide no other details: just the JSON output.`;
+
+    let messages = [
+      { role: 'system', content: '' + system + ''},
+      { role: 'user', content: '' + prompt + '' }
+    ];
+    var r = await callAI(messages, env);
+    return new Response(r.response);
+  } else {
+      response = {
+        error: "Prompt required.",
+      };
+      return Response.json(response);
+    }
+}
+
+async function callAI(messages, env) {
+  let chat = {
+    messages
+  };
+  var model = "@cf/meta/llama-3.2-3b-instruct";
+  // var model = "@cf/meta/llama-3-8b-instruct";
+  let response = await env.AI.run(model, chat);
+  return response;
+}
